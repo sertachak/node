7a063694f0e982322d3aa11823645795db35f52d
Sertac <sertaconurhakbilen@gmail.com>
2020-05-02 15:38:35 +0300

    Yargs

---
+++ require.js
@@ -0,0 +1,60 @@
+const notesProperties = require( './utils')
+
+
+const validator = require('validator')
+const chalk = require('chalk')
+const yargs = require('yargs')
+
+
+console.log(validator.isURL('https://192.168.2.60'))
+
+
+yargs.version('1.1.15')
+
+yargs.command({
+    command:'add',
+    describe:'Add a new note',
+    builder:{
+        noteTitle:{
+            describe:'Title for note',
+            demand: true,
+            type: 'string'         
+        },
+        noteBody:{
+            describe:'Body',
+            demand:true,
+            type:'string'
+        }
+    },
+    handler: function(argv) {
+        notesProperties.addNote( argv.noteTitle, argv.noteBody)
+    }
+})
+
+yargs.command({
+    command:'remove',
+    describe:'Remove a new note',
+    builder:{
+        noteTitle:{
+        describe:'Title of the note gonna removed',
+        demand: true,
+        type: 'string',
+        }
+    },
+
+    handler: function(argv) {
+       notesProperties.removeNote(argv.noteTitle);
+    }
+})
+
+yargs.command({
+    command:'list',
+    describe:'Listing all notes',
+    handler: function() {
+        console.log('All notes are listed')
+    }
+})
+
+
+
+yargs.parse();
---
+++ utils.js
@@ -0,0 +1,52 @@
+const fs = require('fs')
+
+const addNote = (title, body) => {
+    const loadedNotes = loadNotes();
+    console.log('First12' + loadedNotes)
+
+    loadedNotes.push({
+        title: title,
+        body: body
+    })
+    saveNotes(loadedNotes)
+
+    console.log('LAST' + loadedNotes)
+}
+
+
+const removeNote = (title) => {
+    var loadedNotes = loadNotes();
+    console.log(loadedNotes);
+
+    const resultArray = loadedNotes.filter( (note) => {
+       return note.title !== title;
+    })
+    saveNotes(resultArray)
+}
+
+
+const loadNotes = () => {
+    try{
+        const loadedNotes = JSON.parse( fs.readFileSync('notes.json').toString() );
+        return loadedNotes;
+    } catch(e){
+        return [];
+    }
+    
+}
+
+const saveNotes = ( noteData ) => {
+    const jsonData = JSON.stringify(noteData)
+    fs.writeFileSync('notes.json', jsonData)
+}
+
+
+const getNotes = (parameter1, parameter2) =>{
+
+    return parameter1 + parameter2;
+}
+
+module.exports =  {
+    addNote: addNote,
+    removeNote: removeNote
+};
