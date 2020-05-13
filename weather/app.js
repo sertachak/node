a9e9f420048a01bfe0a35391369bd1cf320c7fc8
Sertac <sertaconurhakbilen@gmail.com>
2020-05-13 19:01:39 +0300

    UrlCorrection

--- weather/app.js
+++ weather/app.js
@@ -1,8 +1,8 @@
 const request = require('request');
 
-const url = 'api.openweathermap.org/data/2.5/weather?q={ankara}&appid={b2418f26f3b3b74e1d3817288d21b8e5}';
+const url = 'http://api.openweathermap.org/data/2.5/weather?q=Ankara&units=metric&appid=b2418f26f3b3b74e1d3817288d21b8e5';
 
-request({ url: url}, (err, response) => {
+request({ url: url, json: true }, (err, response) => {
     
     console.log(response);
 })
