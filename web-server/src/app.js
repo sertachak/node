e1e5c5d854ac98e5fc454edeea8c8aa597275ea5
Sertac <sertaconurhakbilen@gmail.com>
2020-06-03 22:20:49 +0300

    Hbs changes

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,11 +1,18 @@
 //usage of nodemon can be helpful
+//nodemon usage -> type 'nodemon <file name>' it only watch changes in that file. If wanted to catch catch other file changes type 'nomdemon <filename> -e(e for extensions) js, hbs'  -> catches changes in js and hbs files
 
 const path = require('path');
 const express = require('express'); 
+const hbs = require('hbs');
 
 const app = express();
 
-app.set('view engine', 'hbs');
+const viewsPath = path.join(__dirname, '../templates/views');
+const partialsPath = path.join(__dirname, '../templates/partials');
+
+app.set('view engine', 'hbs'); // only looks default path views to change that set views
+app.set('views', viewsPath); 
+hbs.registerPartials(partialsPath);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
@@ -29,4 +36,4 @@ app.get('/weather', (req,res) => {
 
 app.listen('3000', () =>{
     console.log('Server is up and running');
-})
+}) 
