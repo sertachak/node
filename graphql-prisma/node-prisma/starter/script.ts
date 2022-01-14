d46cd97eaec02931f706eb63c791a712f0c5b120
sertachak <sertaconurhakbilen@gmail.com>
2022-01-14 22:17:10 +0300

    Working version of prisma

--- graphql-prisma/node-prisma/starter/script.ts
+++ graphql-prisma/node-prisma/starter/script.ts
@@ -4,11 +4,35 @@ const prisma = new PrismaClient()
 
 // A `main` function so that you can use async/await
 async function main() {
+  // ... you will write your Prisma Client queries here
   const allUsers = await prisma.user.findMany({
     include: { posts: true },
   })
   // use `console.dir` to print nested objects
-  console.dir(allUsers, { depth: 1 })
+  console.dir(allUsers, { depth: null })
+
+  await prisma.user.create({
+    data:{
+      name: "coko",
+      email: "coko@treat.com",
+      posts:{
+        create:{title:'Feed me human'}
+      }
+    }
+  }).catch((e) => {
+    console.dir(e);
+  })
+
+  const allUsers1 = await prisma.user.findMany({
+    include:{
+      posts: true
+    }
+  })
+
+  console.dir(allUsers1, {depth: null})
+
+
+
 }
 
 main()
